95b8dfd9a5160e3bba7f2bcdf02ed9c0fd012584
= <=>
2020-01-21 16:42:37 +0800

    添加配置文件库

--- spring-cloud-demo-config/spring-cloud-demo-config-server/src/main/java/org/zxl/springclouddemo/config/server/ConfigServerApplication.java
+++ spring-cloud-demo-config/spring-cloud-demo-config-server/src/main/java/org/zxl/springclouddemo/config/server/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package org.zxl.springclouddemo.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableConfigServer
 @SpringBootApplication
+@RefreshScope
 //@EnableAutoConfiguration
 public class ConfigServerApplication {
     public static void main(String[] args) {
